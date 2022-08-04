1d2ad380ab58ad88b4df07378e336d063b19920b
Raikhana <raikhanadev@outlook.com>
2022-08-03 21:55:58 -0600

    Add project files.

---
+++ NFLTeams/Models/TeamListViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace NFLTeams.Models
+{
+    public class TeamListViewModel : TeamViewModel
+    {
+        public string UserName { get; set; }
+        public List<Team> Teams { get; set; }
+
+        
+        private List<Conference> conferences;
+        public List<Conference> Conferences {
+            get => conferences; 
+            set {
+                conferences = new List<Conference> {
+                    new Conference { ConferenceID = "all", Name = "All" }
+                };
+                conferences.AddRange(value);
+            }
+        }
+
+        private List<Division> divisions;
+        public List<Division> Divisions {
+            get => divisions; 
+            set {
+                divisions = new List<Division> {
+                    new Division { DivisionID = "all", Name = "All" }
+                };
+                divisions.AddRange(value);
+            }
+        }
+
+        
+        public string CheckActiveConf(string c) => 
+            c.ToLower() == ActiveConf.ToLower() ? "active" : "";
+        public string CheckActiveDiv(string d) => 
+            d.ToLower() == ActiveDiv.ToLower() ? "active" : "";
+    }
+}
